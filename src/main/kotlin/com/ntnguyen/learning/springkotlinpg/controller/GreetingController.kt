6cb1ca1e67634fa022d687da3576039dcb6b3c9f
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2024-05-07 17:03:34 +0200

    Greeting Controller

---
+++ src/main/kotlin/com/ntnguyen/learning/springkotlinpg/controller/GreetingController.kt
@@ -0,0 +1,11 @@
+package com.ntnguyen.learning.springkotlinpg.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetingController {
+
+    @GetMapping("/")
+    fun greeting() = "Hello World!!!"
+}
