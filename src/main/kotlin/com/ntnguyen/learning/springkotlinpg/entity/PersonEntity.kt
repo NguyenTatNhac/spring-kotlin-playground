bc667fcd639854fc278976b1129feaff7182823d
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2024-05-08 19:18:56 +0200

    Update Table Name

--- src/main/kotlin/com/ntnguyen/learning/springkotlinpg/entity/PersonEntity.kt
+++ src/main/kotlin/com/ntnguyen/learning/springkotlinpg/entity/PersonEntity.kt
@@ -3,6 +3,8 @@ package com.ntnguyen.learning.springkotlinpg.entity
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "person")
 class PersonEntity(@Id @GeneratedValue var id: Int? = null, val name: String?, val age: Int?)
