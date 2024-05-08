3917b1806c1a621146135ef8fd91a2ff3d7afb4a
Nhac Tat Nguyen <nhactat.nguyen_ext@zooplus.com>
2024-05-08 11:13:55 +0200

    Message data class

--- src/main/kotlin/com/ntnguyen/learning/springkotlinpg/controller/GreetingController.kt
+++ src/main/kotlin/com/ntnguyen/learning/springkotlinpg/controller/GreetingController.kt
@@ -1,6 +1,8 @@
 package com.ntnguyen.learning.springkotlinpg.controller
 
+import com.ntnguyen.learning.springkotlinpg.dto.Message
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -8,4 +10,7 @@ class GreetingController {
 
     @GetMapping("/")
     fun greeting() = "Hello World!!!"
+
+    @GetMapping("/hi")
+    fun sayHi(@RequestParam("name") name: String) = Message(1, "Hi $name")
 }
---
+++ src/main/kotlin/com/ntnguyen/learning/springkotlinpg/dto/Message.kt
@@ -0,0 +1,3 @@
+package com.ntnguyen.learning.springkotlinpg.dto
+
+data class Message(val id: Int?, val message: String)
